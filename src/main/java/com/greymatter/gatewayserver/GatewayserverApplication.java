e9944ce1d63adf36f3cd962b26fd5293df4a6eb2
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2024-10-20 20:07:29 -0500

    Adding Filters

--- src/main/java/com/greymatter/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/greymatter/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,43 @@ package com.greymatter.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/greymatter/accounts/**")
+                        .filters( f -> f.rewritePath("/greymatter/accounts/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/greymatter/loans/**")
+                        .filters( f -> f.rewritePath("/greymatter/loans/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/greymatter/cards/**")
+                        .filters( f -> f.rewritePath("/greymatter/cards/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS"))
+                .route(p -> p
+                .path("/greymatter/identity/**")
+                .filters( f -> f.rewritePath("/greymatter/identity/(?<segment>.*)","/${segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                .uri("lb://IDENTITY")).build();
+
     }
 
 }
