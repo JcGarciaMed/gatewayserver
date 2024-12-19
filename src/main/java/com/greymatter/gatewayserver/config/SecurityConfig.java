16adb6bcf6a1d628b08c2bd0bcb13ccf358bf3c4
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2024-12-18 20:02:35 -0500

    Implement authorization gateway server using roles

--- src/main/java/com/greymatter/gatewayserver/config/SecurityConfig.java
+++ src/main/java/com/greymatter/gatewayserver/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,12 +25,21 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
         log.debug("Configuring SecurityWebFilterChain");
         return serverHttpSecurity.authorizeExchange(exchanges-> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/greymatter/accounts/**").authenticated()
-                        .pathMatchers("/greymatter/cards/**").authenticated()
-                        .pathMatchers("/greymatter/loans/**").authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                        .pathMatchers("/greymatter/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/greymatter/cards/**").hasRole("CARDS")
+                        .pathMatchers("/greymatter/loans/**").hasRole("LOANS")
+                .pathMatchers("/greymatter/identity/**").hasRole("IDENTITY"))
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> 
+                        oAuth2ResourceServerSpec.jwt(jwtSpec ->
+                                jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())))
                 .csrf(csrfSpec -> csrfSpec.disable()).build();
-                //.oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
-        //serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
